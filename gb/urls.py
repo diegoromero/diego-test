21ef959e3ca1c820e9d66be9471c66b360cbc034
Diego Romero <diego.romerop@gmail.com>
2014-03-15 12:21:34 -0430

    home view

--- gb/urls.py
+++ gb/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, url
 from gb import views
 
-urlpatterns = patterns('views',
-    url(r'^$', home),
+urlpatterns = patterns('',
+    url(r'^$', views.home),
 )
