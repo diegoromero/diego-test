8d6fd539ff023783e587d0b0e7e3e86546aa10dc
Diego Romero <diego.romerop@gmail.com>
2014-03-18 15:37:38 -0430

    dummy

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'hellodjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.dummy',
         'NAME': '',
         'USER': '',
         'PASSWORD': '',
