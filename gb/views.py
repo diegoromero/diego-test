8ba5e2939efedc05e0df06d8e79c534853cde419
diegoromero <diego.romerop@gmail.com>
2014-03-18 11:33:41 -0430

    user views

--- gb/views.py
+++ gb/views.py
@@ -5,6 +5,10 @@ from gb.models import User
 
 # Create your views here.
 def home(request):
+    if request.user.is_authenticated():
+        '''If the user is already registered it goes
+        to the manager view'''
+        return redirect('www.google.com')
     User.create_user(username='c0', email='c@0.com', password='c0')
     return render(request, 'home_index.html',
                   {'title': 'Welcome'})
