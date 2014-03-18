b11e139439b700596747fe5270b9f6d3672ddc58
diegoromero <diego.romerop@gmail.com>
2014-03-18 10:49:33 -0430

    views User

--- gb/views.py
+++ gb/views.py
@@ -5,5 +5,7 @@ from gb.models import User
 
 # Create your views here.
 def home(request):
+    if request.method == 'POST':
+        User.create_user(username=username, email=email, password=password)
     return render(request, 'home_index.html',
                   {'title': 'Welcome'})
