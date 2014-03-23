3e55c3fa18748efaf1ad52e906074e55d46a25d9
Diego Romero <diego.romerop@gmail.com>
2014-03-23 11:55:56 -0430

    clean settings

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -152,9 +152,11 @@ TEMPLATE_LOADERS = (
 AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+DEFAULT_FILE_STORAGE = 'hellodjango.s3utils.MediaS3BotoStorage'
 STATICFILES_STORAGE = 'hellodjango.s3utils.StaticS3BotoStorage'
-S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
-STATIC_DIRECTORY = 'static/'
+S3_URL = 'http://%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+STATIC_DIRECTORY = '/static/'
+MEDIA_DIRECTORY = '/media/'
 STATIC_URL = S3_URL + STATIC_DIRECTORY
-MEDIA_URL = S3_URL + 'media/'
+MEDIA_URL = S3_URL + MEDIA_DIRECTORY
+
