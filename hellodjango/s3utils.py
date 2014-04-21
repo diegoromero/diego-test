c02ed7ea81533fd1b7b88cdaebbabf5f3b4cae78
diegoromero <diego.romerop@gmail.com>
2014-04-21 10:53:09 -0430

    no success redirect

--- hellodjango/s3utils.py
+++ hellodjango/s3utils.py
@@ -13,7 +13,6 @@ policy_document = '''
     {"bucket": "diego-test_bucket"}, 
     ["starts-with", "$key", "uploads/"],
     {"acl": "public-read"},
-    {"success_action_redirect": "http://diego-test.herokuapp.com/"},
     ["starts-with", "$Content-Type", ""],
     ["content-length-range", 0, 1048576]
   ]
