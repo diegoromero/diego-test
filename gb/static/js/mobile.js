c8d9f7f17233622dc5b6c6c5034f3a6e417055a2
diegoromero <diego.romerop@gmail.com>
2014-04-22 11:54:58 -0430

    changes

--- gb/static/js/mobile.js
+++ gb/static/js/mobile.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
 
   if (navigator.userAgent.match(/Android/i)) {
+	$('#tit').text('android');
     window.scrollTo(0,0); // reset in case prev not scrolled  
     var nPageH = $(document).height();
     var nViewH = window.outerHeight;
     if (nViewH > nPageH) {
-		nViewH -= 250;
+		nViewH = nViewH / window.devicePixelRatio;
 		$('BODY').css('height',nViewH + 'px');
     }
     window.scrollTo(0,1);
