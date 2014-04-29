6e72467464aefe4482b064f840d767b9687353d1
Diego Romero <diego.romerop@gmail.com>
2014-04-29 16:14:26 -0430

    print request.session

--- gb/views.py
+++ gb/views.py
@@ -20,7 +20,7 @@ def home(request):
     
     return render(request, 'home_index.html',
                   {'title': 'Welcome',                   
-                   'request': request,
+                   'session': request.session,
                    'bill_n': request.session['bill_n']})
 
 def mobile(request):
