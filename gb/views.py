53d79bddb0e655e9c6c77f1cea89ccbd7d1c56d5
diegoromero <diego.romerop@gmail.com>
2014-04-30 09:39:17 -0430

    expire print

--- gb/views.py
+++ gb/views.py
@@ -17,12 +17,19 @@ def home(request):
         bills = dao.get_bills(client_id)
         bills %= 10000
         request.session['bill_n'] = bills
+
+    
+    expire = []
+    expire.append(request.session.get_expiry_date())
+    expire.append(request.session.get_expiry_age())
+    expire.append(get_expire_at_browser_close())
     
     return render(request, 'home_index.html',
                   {'title': 'Welcome',                   
                    'request': request,
                    'session': str(request.session),
-                   'bill_n': request.session['bill_n']})
+                   'bill_n': request.session['bill_n'],
+                   'expire': expire})
 
 def mobile(request):
     '''Mobile view'''
