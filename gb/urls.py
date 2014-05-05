e0bfeea59f633b87444803ea618d59115d55e118
diegoromero <diego.romerop@gmail.com>
2014-05-05 11:34:57 -0430

    views.

--- gb/urls.py
+++ gb/urls.py
@@ -7,8 +7,8 @@ from django_sse.redisqueue import RedisQueueView
 
 urlpatterns = patterns('',
     url(r'^home/$', views.home),
-    url(r'^home1/$', Home1.as_view(), name='home1'),
-    url(r'^home2/$', Home2.as_view(), name='home2'),
+    url(r'^home1/$', views.Home1.as_view(), name='home1'),
+    url(r'^home2/$', views.Home2.as_view(), name='home2'),
 
     url(r'^events1/$', MySseEvents.as_view(), name='events1'),
     url(r'^events2/$', RedisQueueView.as_view(), name='events2'),
